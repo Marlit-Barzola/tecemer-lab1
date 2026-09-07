1a64c0711235f463187487173d698fbcc2d7ca25
Marlit Barzola Chancasanampa <e_2025100769B@uncp.edu.pe>
2026-09-07 09:59:09 -0500

    stylre: aplica formateo PEP 8 con Black y agrega pyproject..toml

---
+++ src/tecemer_lab1/app.py
@@ -0,0 +1,15 @@
+import requests
+
+try:
+    # Todo este bloque debe tener sangría (indentación)
+    respuesta = requests.get(
+        "https://official-joke-api.appspot.com/random_joke", timeout=5
+    )
+    respuesta.raise_for_status()
+    datos = respuesta.json()
+    print(datos["setup"])
+    print(datos["punchline"])
+except requests.RequestException as error:
+    # Esta línea también debe tener sangría
+    print(f"No se pudo obtener el chiste: {error}")
+# Cambio para Pull Request
